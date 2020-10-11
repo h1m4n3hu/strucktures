c44d793f8bb20cb42ca4d4043b0053a3e87a9b35
himanshusoni2708 <himanshusoni2708@gmail.com>
2020-10-11 18:26:31 +0530

    cir

--- clist.py
+++ clist.py
@@ -7,31 +7,35 @@ class Clist:
     def __init__(self):
         self.head=None
 
-    def append(self, data):
+    def append(self, value):
         if not self.head:
-            self.head = data
-            self.head.next = self.head
+            self.head=value
+            self.head.next=self.head
         else:
-            new_node = data
-            cur = self.head
-            while cur.next != self.head:
-                cur = cur.next
-            cur.next = new_node
-            new_node.next = self.head
+            #new_node=data
+            #cur=self.head
+            first=self.head
+            while first:
+                first=first.next
+                if first.next==self.head:
+                    break
+            first.next=value
+            value.next=self.head
 
     def show(self):
         first = self.head
         while first:
+            print(first.value)
             first=first.next
-            print("hh")
             if first==self.head:
                 break
 
+
 """    def show(self):
-    first=self.head
-    while first.next!=self.head:
-        print("h")
-        first=first.next"""
+        first=self.head
+        while first.next!=self.head:
+            print("h")
+            first=first.next"""
 
 
 
@@ -49,5 +53,7 @@ l.append(Node("a"))
 l.append(Node("b"))
 l.append(Node("c"))
 l.append(Node("d"))
+l.append(Node("e"))
+l.append(Node("f"))
 
 l.show()
