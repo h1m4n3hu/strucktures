bcef0417b26fdbc8a4df3290914111579c39ec6c
himanshusoni2708 <himanshusoni2708@gmail.com>
2020-10-12 06:46:01 +0530

    cir

--- clist.py
+++ clist.py
@@ -12,8 +12,6 @@ class Clist:
             self.head=value
             self.head.next=self.head
         else:
-            #new_node=data
-            #cur=self.head
             first=self.head
             while first:
                 first=first.next
@@ -22,24 +20,83 @@ class Clist:
             first.next=value
             value.next=self.head
 
+    def prepend(self,value):
+        if not self.head:
+            value.next=value
+        else:
+            first=self.head
+            k=first
+            while first.next!=self.head:
+                first=first.next
+            first.next=value
+            value.next=k
+        self.head=value
+
+    def pop(self):
+        first=self.head
+        while first.next!=self.head:
+            last=first
+            first=first.next
+        last.next=self.head
+        first.next=None
+
+    def poptop(self):
+        first=self.head
+        second=first.next
+        first.next=None
+        self.head=second
+        k=self.head
+        while k.next:
+            print(k.value,"k")
+            last=k
+            k=k.next
+            if k==self.head:
+                break
+        last.next=second
+
+    def remove(self,pos):
+        i=0
+        first=self.head.next
+        while i<pos-1:
+            prev=first
+            first=first.next
+            i+=1
+        prev.next=first.next
+        first.next=None
+
+    def index(self,data):
+        i=0
+        first=self.head
+        while first:
+            if first.value==data:
+                return i
+            first=first.next
+            i+=1
+            if first==self.head:
+                break
+
+    def delete(self,data):
+        ii=self.index(data)
+        self.remove(ii)
+
     def show(self):
-        first = self.head
+        first=self.head
         while first:
             print(first.value)
             first=first.next
             if first==self.head:
                 break
-
+        #while first.next!=self.head:
+            #print(first.value)
+            #first=first.next
 
 """    def show(self):
         first=self.head
         while first.next!=self.head:
             print("h")
-            first=first.next"""
+            first=first.next
 
-
-
-"""    def show(self):
+    def show(self):
         first=self.head
         while first:
             print(first.value)
@@ -55,5 +112,6 @@ l.append(Node("c"))
 l.append(Node("d"))
 l.append(Node("e"))
 l.append(Node("f"))
+l.delete("c")
 
 l.show()
---
+++ extra.py
@@ -0,0 +1,63 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def prepend(self, data):
+        new_node = Node(data)
+        cur = self.head
+        new_node.next = self.head
+
+        if not self.head:
+            new_node.next = new_node
+        else:
+            while cur.next != self.head:
+                cur = cur.next
+            cur.next = new_node
+        self.head = new_node
+
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+            self.head.next = self.head
+        else:
+            new_node = Node(data)
+            cur = self.head
+            while cur.next != self.head:
+                cur = cur.next
+            cur.next = new_node
+            new_node.next = self.head
+
+    def print_list(self):
+        cur = self.head
+
+        while cur:
+            print(cur.data)
+            cur = cur.next
+            if cur == self.head:
+                break
+
+    def poptop(self):
+        first=self.head
+        second=first.next
+        first.next=None
+        self.head=second
+        k=self.head
+        while k:
+            print(k.data,"k")
+            k=k.next
+            if k.next==self.head:
+                break
+
+cllist = CircularLinkedList()
+cllist.append("C")
+cllist.append("D")
+cllist.prepend("B")
+cllist.prepend("A")
+cllist.poptop()
+cllist.print_list()
